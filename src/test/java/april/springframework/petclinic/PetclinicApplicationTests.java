69c99faa43f81ad43fa5b7cbdc822df935807ef3
aprilhoward <apehow111@gmail.com>
2020-09-29 07:22:42 -0400

    Initial Project Add

---
+++ src/test/java/april/springframework/petclinic/PetclinicApplicationTests.java
@@ -0,0 +1,14 @@
+package april.springframework.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class PetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
